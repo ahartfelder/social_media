fcb8ec8052d63111ab016c0aaaa0d29a88017e04
ahartfelder <ahartfelder@gmail.com>
2022-05-26 10:06:36 -0300

    home and articles

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Helper for articles
+module ArticlesHelper
+end
---
+++ db/migrate/20220526125356_create_articles.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Database for articles
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
