9f1c4949139f4aba01ed8dc02ecdde70a2bd44d8
ahartfelder <ahartfelder@gmail.com>
2022-05-26 16:23:58 -0300

    style article and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
   def active?(current)
     request.path == current ? ' active' : nil
   end
+
+  def form_errors(object)
+    render 'shared/form_errors', object: object
+  end
 end
