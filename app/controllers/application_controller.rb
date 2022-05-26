91272a52abab1b368627d0d380223a840d1c3db7
ahartfelder <ahartfelder@gmail.com>
2022-05-26 09:52:30 -0300

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Controls the application
 class ApplicationController < ActionController::Base
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Helper for views
 module ApplicationHelper
 end
