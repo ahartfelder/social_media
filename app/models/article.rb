1fbf1ce5cd374b722f602fdacd5f6ccb9bdc10d4
ahartfelder <ahartfelder@gmail.com>
2022-05-27 08:59:47 -0300

    fix: comment model and fixtures

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@
 
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  
+
   validates :title, presence: true
   validates :body, presence: true, length: { in: 5..300 }
 end
