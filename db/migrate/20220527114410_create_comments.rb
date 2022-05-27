b5ab180cf66fe075b7f6e491f39752d9d76fc254
ahartfelder <ahartfelder@gmail.com>
2022-05-27 08:54:18 -0300

    comments model and fixtures

---
+++ db/migrate/20220527114410_create_comments.rb
@@ -0,0 +1,11 @@
+# Database for Comments
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :message, null: false
+      t.references :article, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
